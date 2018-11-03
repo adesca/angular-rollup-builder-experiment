c7d8b5250810b563b121ea3ae60324afbd898a3a
Adeolu babatunde <adeolubabatunde@localhost.localdomain>
2018-11-03 09:59:34 -0500

    Added ability to import modules to angular container

--- container/src/app/app.component.ts
+++ container/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {System} from 'systemjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private SystemJS: System = window['System'];
   title = 'container';
+
+  ngOnInit(): void {
+   this.ngOnInitAsync().then(() => console.log('load was successful')
+     , err => console.log('Load failed for this reason', err));
+
+  }
+
+  async ngOnInitAsync() {
+    const module: Module<ExternalDoStuffModule> = await this.SystemJS.import('/assets/out.js');
+    console.log('imported module ', module);
+    module.default.doStuff();
+  }
+
+
+}
+
+class Module<T> {
+  default: T;
+}
+
+class ExternalDoStuffModule {
+  doStuff: () => void;
 }
